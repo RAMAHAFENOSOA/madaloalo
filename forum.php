9210d0e925b9088728129bf764b09f48f79475e5
RAMAHAFENOSOA <rtarantsoa@gmail.com>
2023-08-22 15:55:01 +0300

    forumm

---
+++ forum.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Voyagez à Madagascar</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/madaloalo-high-resolution-color-logo.png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body style="display:flex">
+<div class="container-fluid">
+    <div class="row flex-nowrap" >
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0" style="background-color:#178ca4;position:fixed;z-index:3">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span class="fs-5 d-none d-sm-inline">Forum</span>
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                <li class="nav-item">
+                        <a href="#" class="nav-link text-white align-middle px-0">
+                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none text-white d-sm-inline">Home</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle text-white">
+                            <i class="bi bi-chat-left-text-fill"></i> <span class="ms-1 d-none d-sm-inline text-white">boîte à questions</span> </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link px-0 align-middle text-white">
+                        <i class="bi bi-chat-left-dots-fill"></i> <span class="ms-1 d-none d-sm-inline text-white">chat</span> </a>
+                    </li>
+                </ul>
+                <hr>
+                <div class="dropdown pb-4">
+                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle text-white" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                        <span class="d-none d-sm-inline mx-1">loser</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col py-3" style="padding-left:25%;" >
+            <!-- message -->
+            <div class="container-fluid mt-100">
+
+
+                            <div class="row">
+                               <div class="col-md-12">
+                                   <div class="card mb-4">
+                                       <div class="card-header">
+                                           <div class="media flex-wrap w-100 align-items-center" style="display:flex;"> 
+                                               <img src="https://i.imgur.com/iNmBizf.jpg" class="ui-w-40 rounded-circle" alt="">
+                                               <div class="media-body ml-3" style="margin-left:15px"> 
+                                                  <a href="javascript:void(0)" data-abc="true">Tom Harry</a>
+                                               <div class="text-muted small">13 days ago</div>
+                                           </div>
+                                       </div>
+                                   </div>
+                                       <div class="card-body">
+                                          <p>soratra</p>
+                                       </div>
+                                       <div class="card-footer d-flex flex-wrap justify-content-between align-items-center px-0 pt-0 pb-3" >
+                                           <div></div>
+                                           <div class="px-4 pt-3" > <button type="button" class="btn btn-primary" style="position:relative"><i class="ion ion-md-create"></i>&nbsp;Reply</button> </div>
+                                       </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+</body>
+
+</html>
+
