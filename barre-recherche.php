b05e0d88d40ea81bd771a0555470ff1606e67fe0
RAMAHAFENOSOA <RAMAHAFENOSOA>
2023-08-24 23:17:03 +0300

    barrederecherchemodif

--- barre-recherche.php
+++ barre-recherche.php
@@ -33,17 +33,40 @@
   }
 
   .search-section-header {
+    text-align: center;
+    align-items: center;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     padding: 12px;
   }
 
+  input{
+    position: relative;
+    display: inline-block;
+    font-size: 20px;
+    box-sizing: border-box;
+    transition: .5s;
+  }
+  form{
+    width: 75%;
+    height:50%;
+  }
+
   .search-button {
+    border-radius: 0px 25px 25px 0px;
+    position: relative;
+    left: -5px;
+    height: 50px;
+    width: 150px;
     background: #68A4C4;
     border: 0;
     padding: 3px 23px;
     color: #fff;
+  }
+
+  .search-button:hover{
+    background: #1e4356;
   }
 
   @keyframes slideDown {
@@ -56,19 +79,23 @@
   }
 </style>
 
-<div id="searchOverlay"></div>
+<div id="searchOverlay">
 
-<section id="slide-down-section">
-<div class="search-section-header">
-    <!-- <h5 style="text-align: center;">Recherche</h5> -->
-    <form class="search-form">
-        <input type="text" class="search-input" placeholder="Ecrivez votre recherche...">
-        <button type="submit" class="search-button">Recherche</button>
-    </form>
-    <button type="button" class="close-button" id="close-button" aria-label="Fermer" style="font-size:20px;">
-    <span aria-hidden="true">&times;</span>
-    </button>
+  <section id="slide-down-section" style="width: 100%;height: 20%; ">
+    <div class="search-section-header">
+        
+        <form class="search-form" method="GET" action="afficherRecherche.php" >
+            <input type="text" class="search-input" name="s" placeholder="Recherche..." autocomplete="off" style="background: #fff;width: 50%;height: 50px;border: 2px solid #68A4C4;outline: none;padding: 0 25px;border-radius: 25px 0 0 25px;">
+            <input type="submit" class="search-button"  name="envoyer" value="Recherche">
+          
+        </form>
+        <button type="button" class="close-button" id="close-button" aria-label="Fermer" style="font-size:20px; height: 30px;">
+        <span aria-hidden="true">&times;</span>
+        </button>
+    </div>      
+  </section>
 </div>
+  
 </section>
 <script>
   const searchButton = document.getElementById('search-button');
